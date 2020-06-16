2864d24e1ee23386043e9c857d52865e6486f6b4
Falko Riemenschneider <riemenschneider@doctronic.de>
2020-06-16 16:06:50 +0200

    Activate num layer after sending a ticket number

--- keymaps/friemen/keymap.c
+++ keymaps/friemen/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_DEL,   \
     _______,   KC_F11,    KC_F12,    _______,   _______,  OSL(_TEXT), _______,   _______,   KC_UP,     KC_PGUP,   KC_PGDN,   CTL_PLUS, \
     _______,   _______,   _______,   _______,   _______,   _______,   KC_HOME,   KC_LEFT,   KC_DOWN,   KC_RIGHT,  KC_END,    CTL_MINS, \
-    RESET,     _______,   _______,   _______,   KC_MPLY,   KC_MNXT,   KC_HOME,   KC_END,    KC_VOLD,   KC_VOLU,   KC_MUTE,   _______,  \
+    RESET,     _______,   _______,   KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_HOME,   KC_END,    KC_VOLD,   KC_VOLU,   KC_MUTE,   _______,  \
                                                 _______,   _______,   _______,   _______  \
   ),
 
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUM] = LAYOUT(
-    _______,   KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      _______, \
+    TG(_NUM),  KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      _______, \
     _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_4,      KC_5,      KC_6,      KC_PLUS,   KC_ASTR, \
     _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_1,      KC_2,      KC_3,      KC_MINS,   KC_SLSH, \
     _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_0,      KC_COMM,   KC_DOT,    KC_EQL,    KC_ENT, \
@@ -202,11 +202,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   case M_BFNBGBM:
     if (record->event.pressed) {
       SEND_STRING("BFNBGBM-");
+      layer_on(_NUM);
     }
     return false;
   case M_DTMS:
     if (record->event.pressed) {
       SEND_STRING("DTMS-");
+      layer_on(_NUM);
     }
     return false;
   default:
